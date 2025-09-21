009db618acdb1c335ca071f1dc56fd9f719d109d
Ahmed Roshdy <ahmedroshdy654@gmail.com>
2025-09-21 16:32:05 +0300

    Added: doctors home page

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,15 +1,11 @@
 import 'package:doctor/core/helpers/spacing.dart';
-import 'package:doctor/core/theming/colors.dart';
-import 'package:doctor/features/home/logic/home_cubit.dart';
-import 'package:doctor/features/home/logic/home_state.dart';
-import 'package:doctor/features/home/ui/widgets/doctor_speciality.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'widgets/doctor_banner.dart';
-import 'widgets/doctor_speciality_list.dart';
+import 'widgets/doctors_banner.dart';
+import 'widgets/doctors_speciality.dart';
 import 'widgets/home_top_bar.dart';
+import 'widgets/specializations_and_doctors_bloc_builder.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -17,67 +13,20 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ColorsManager.white,
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Container(
           width: double.infinity,
-          margin: EdgeInsets.fromLTRB(10, 16, 10, 20),
+          margin: const EdgeInsets.fromLTRB(20.0, 16.0, 20.0, 28.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-
             children: [
               const HomeTopBar(),
               const DoctorBanner(),
               verticalSpace(24.h),
               const DoctorSpeciality(),
-              verticalSpace(16.h),
-              const DoctorSpecialityList(),
-              verticalSpace(23.h),
-              BlocBuilder<HomeCubit, HomeState>(
-                buildWhen:
-                    (previous, current) =>
-                        current is SpecializationsLoading ||
-                        current is SpecializationsSuccess ||
-                        current is SpecializationsError,
-                builder: (context, state) {
-                  return state.maybeWhen(
-                    specializationsLoading: () {
-                      return const SizedBox(
-                        height: 100,
-                        child: Center(child: CircularProgressIndicator()),
-                      );
-                    },
-                    specializationsSuccess: (specializationsResponseModel) {
-                      var specializationsList =
-                          specializationsResponseModel.specializationDataList;
-                      return Expanded(
-                        child: Column(
-                          children: [
-                            DoctorSpecialityList(),
-                            verticalSpace(8),
-                            const DoctorSpeciality(),
-                          ],
-                        ),
-                      );
-                    },
-                    specializationsError: (errorHandler) {
-                      return const SizedBox.shrink();
-                      // return Center(
-                      //   child: Text(
-                      //     errorHandler.,
-                      //     style: TextStyle(color: ColorsManager.red),
-                      //   ),
-                      // );
-                    },
-                    orElse: () {
-                      return const SizedBox.shrink();
-                    },
-                  );
-                },
-              ),
-              // const DoctorsListViewItem(),
-              verticalSpace(12.h),
-              const DoctorSpecialityList(),
+              verticalSpace(18),
+              const SpecializationsAndDoctorsBlocBuilder(),
             ],
           ),
         ),
